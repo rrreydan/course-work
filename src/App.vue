be23babcfe734999a5ec120a8f8466d35f9d3913
Andrey Deykun <rrreydan@gmail.com>
2024-03-27 19:55:07 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TheHeader from '@/components/global/TheHeader.vue'
+</script>
+
+<template>
+  <TheHeader />
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div>
+    Hello
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
