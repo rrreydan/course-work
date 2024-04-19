7c7183be199bce980bfb18ab1d3602d06cdcf94b
Andrey Deykun <rrreydan@gmail.com>
2024-04-19 17:44:30 +0300

    delete text buttons in top navbar

--- src/components/global/TheTopNavbar.vue
+++ src/components/global/TheTopNavbar.vue
@@ -1,40 +1,8 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import LoginModal from '@/components/modals/LoginModal.vue'
 import { useUsersStore } from '@/store/users'
 
 const usersStore = useUsersStore()
-
-const textButtons = ref([
-  {
-    id: 1,
-    text: 'Главная',
-    href: '/',
-    isToggle: true
-  },
-  {
-    id: 2,
-    text: 'Автовокзал',
-    href: '#',
-    isToggle: false
-  },
-  {
-    id: 3,
-    text: 'Контакты',
-    href: '#',
-    isToggle: false
-  }
-])
-
-const changeToggle = (id: number): void => {
-  for (let button of textButtons.value) {
-    if (button.id === id) {
-      button.isToggle = true
-    } else {
-      button.isToggle = false
-    }
-  }
-}
 
 const logoutHandler = () => {
   usersStore.logoutUser()
@@ -54,19 +22,6 @@ const logoutHandler = () => {
         />
         <p>Andrey Bus</p>
       </router-link>
-      <ul class="text-buttons">
-        <li
-          v-for="button in textButtons"
-          :key="button.id"
-          class="hover-underline-animation"
-          :class="{ toggle: button.isToggle }"
-          @click="changeToggle(button.id)"
-        >
-          <router-link :to="button.href">
-            {{ button.text }}
-          </router-link>
-        </li>
-      </ul>
       <div class="other-buttons">
         <LoginModal v-if="!usersStore.user.status.loggedIn" />
         <div
@@ -111,13 +66,6 @@ const logoutHandler = () => {
   gap: 0.5rem;
   font-size: 1.5rem;
   font-weight: 700;
-}
-
-.text-buttons {
-  display: flex;
-  align-items: center;
-  gap: 2rem;
-  list-style: none;
 }
 
 .hover-underline-animation {
